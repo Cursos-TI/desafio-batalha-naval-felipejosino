95a3f8c2af4c11a1c94077e0a21b44d85288cbcf
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2025-03-05 16:32:25 +0000

    Finalizado desafio novato.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,61 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[10][10];
+
+    // Inicializando o tabuleiro preenchendo com 0
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
+        
+    }
+
+    // Adicionando navio na horizonrtal
+    // tabuleiro[2][3] = 3;
+    // tabuleiro[2][4] = 3;
+    // tabuleiro[2][5] = 3;
+
+    for (int i = 3; i < 6; i++)
+    {
+        tabuleiro[2][i] = 3;
+    }    
+
+    // Adicionando navio na vertical
+    for (int i = 5; i < 8; i++)
+    {
+        tabuleiro[i][7] = 3;
+    }   
+
+    printf("------------------------\n");
+    printf("TABULEIRO BATALHA NAVAL \n");
+    printf("------------------------\n");
+    printf("   ");
+
+    // Exibe valores do vetor Linha
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%c ", linha[i]);
+    }
+
+    printf("\n"); // Quebra de linha
+    
+    // Exibe o tabuleiro
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d |", i + 1); // Exibe números das linhas
+
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        
+        printf("\n"); //Quebra de linha
+    }
+    
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
