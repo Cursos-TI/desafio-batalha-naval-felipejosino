eab7d48a57399ebf9faa8d5833e67c1ac5ce3975
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2025-03-12 00:26:16 +0000

    Add. primiera habilidade especial

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,6 +4,30 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+// Desenhando Cone
+void desenharCone(int tabuleiro[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 2 || (i == 1 && j >= 1 && j <= 3) || (i == 0 && j == 2)) {
+                tabuleiro[i][j] = 1;
+            } else {
+                tabuleiro[i][j] = 0;
+            }
+        }
+    }
+}
+
+// Exibir Tabuleiro
+void exibirTabuleiro(int tabuleiro[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
@@ -11,6 +35,8 @@ int main() {
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int tabuleiro[10][10];
+
+    int tabuleiroCone[5][5];
 
     // Inicializando o tabuleiro preenchendo com 0
     for (int i = 0; i < 10; i++)
@@ -84,6 +110,9 @@ int main() {
         printf("\n"); //Quebra de linha
     }
 
+    printf("\n");
+    printf("Habilidades Especiais \n");
+    
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
@@ -94,6 +123,9 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
+    desenharCone(tabuleiroCone);
+    printf("Tabuleiro Cone:\n");
+    exibirTabuleiro(tabuleiroCone);
     
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
